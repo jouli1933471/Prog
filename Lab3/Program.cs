25695436f271c76f3bfabcfa194a5a9cde36834f
jouli1933471 <54861260+jouli1933471@users.noreply.github.com>
2019-11-22 15:21:37 -0500

    Add files via upload

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -1,136 +1,91 @@
 ﻿using System;
 
-namespace lab3_atelier2
+namespace lab3_atelier1
 {
     class Program
     {
-        static Random generateur = new Random();
+        static int[] CreerTableau(ref string laPhrase)
+        {
+            string laPhraseM = laPhrase.ToLower();
+            int[] tabLettre = new int[26];
+
+            for (int i = 0; i < laPhraseM.Length; i++)
+            {
+                int indice = (int)laPhraseM[i] - 97;
+                if (indice >= 0 && indice < 26)
+                    tabLettre[indice]++;
+            }
+            return tabLettre;
+        }
+        static void AfficherNbMots(ref string laPhrase)
+        {
+            string[] tabMots = laPhrase.Split(' ');
+
+            Console.WriteLine(tabMots.Length);
+        }
+        static void AfficherLettreNbDeFois(ref string laPhrase)
+        {
+            int[] tabAlphabet = CreerTableau(ref laPhrase);
+
+            Console.WriteLine("Voice vos résultats.");
+            for(int i=0;i< tabAlphabet.Length;i++)
+            {
+                char lettre = Convert.ToChar(i + 97);
+                Console.WriteLine("la lettre "+lettre+" est "+ tabAlphabet[i] + " fois dans votre phrase");
+            }
+        }
+        static void AfficherLettrePlus(ref string laPhrase)
+        {
+            int[] tabAlphabet = CreerTableau(ref laPhrase);
+
+            int lettrePlusUtiliser = 0;
+            int position = 0;
+             
+            for(int i =0;i<tabAlphabet.Length;i++)
+            {
+                if (tabAlphabet[i] < lettrePlusUtiliser)
+                {
+                    lettrePlusUtiliser = tabAlphabet[i];
+                    position = i;
+                }
+            }
+            char lettre = Convert.ToChar(position + 97);
+            Console.WriteLine("la lettre qui apparait le plus de fois est "+lettre);
+        }
+        static void Encoder(ref string laPhrase)
+        {
+            string tabLettre = "";
+            for (int i = 0; i < laPhrase.Length; i++)
+            {
+                int noLettre = (int)laPhrase[i];
+                tabLettre += char.ConvertFromUtf32((noLettre + 2));
+            }
+            Console.WriteLine("voici votre phrase "+tabLettre);
+        }
+        static void AfficherMenu()
+        {
+            Console.WriteLine("1. Affiche le nombre de mots dans votre phrase ");
+            Console.WriteLine("2. Affiche combien de fois chaque apparrait dans votre phrase");
+            Console.WriteLine("3. Affiche la lettre qui est le plus de fois dans votre phrase");
+            Console.WriteLine("4. Permet d'encoder votre phrase ");
+        }
         static void Main(string[] args)
         {
-            string vie6 = @" 
-                              +---+
-                              |   |
-                              O   |
-                                  |
-                                  |
-                                  |
-                            =========";
-            string vie5 = @"  
-                              +---+
-                              |   |
-                              O   |
-                              |   |
-                                  |
-                                  |
-                            =========";
-            string vie4 = @"  
-                              +---+
-                              |   |
-                              O   |
-                             /|   |
-                                  |
-                                  |
-                            =========";
-            string vie3 = @"  
-                              +---+
-                              |   |
-                              O   |
-                             /|\  |
-                                  |
-                                  |
-                            =========";
-            string vie2 = @"  
-                              +---+
-                              |   |
-                              O   |
-                             /|\  |
-                             /    |
-                                  |
-                            =========";
-            string vie1 = @"  
-                              +---+
-                              |   |
-                              O   |
-                             /|\  |
-                             / \  |
-                                  |
-                            =========";
+            Console.WriteLine("Inscriver un phrase");
+            string laPhrase = Console.ReadLine();
 
-            string vie0 = @"     
-                                  _____          __  __ ______    ______      ________ _____  _            
-                                 / ____|   /\   |  \/  |  ____|  / __ \ \    / /  ____|  __ \| |           
-                                | |  __   /  \  | \  / | |__    | |  | \ \  / /| |__  | |__) | |           
-                                | | |_ | / /\ \ | |\/| |  __|   | |  | |\ \/ / |  __| |  _  /| |           
-                                | |__| |/ ____ \| |  | | |____  | |__| | \  /  | |____| | \ \|_|           
-                                 \_____/_/    \_\_|  |_|______|  \____/   \/   |______|_|  \_(_) ";
-
-
-
-
-            string[] tabMots = {"lama","poney","chien","chat","oiseau","mouche","ours","lapin","vache","cochon"};
-            string motATrouve = tabMots[generateur.Next(0,10)];
-            char[] tabJeu = new char[motATrouve.Length];
-            for(int i=0;i<tabJeu.Length;i++)
+            AfficherMenu();
+            int choixMenu = Convert.ToInt32(Console.ReadLine());
+            switch (choixMenu)
             {
-                tabJeu[i] = '_';
+                case 1: AfficherNbMots(ref laPhrase); break;
+                case 2: AfficherLettreNbDeFois(ref laPhrase); break;
+                case 3: AfficherLettrePlus(ref laPhrase); break;
+                case 4: Encoder(ref laPhrase); break;
+                default: Console.WriteLine("Entrer un choix existant"); break;
             }
-            int vie = 7;
-            bool motTrouver = false;
-            bool lettreTrouver = false;
-            bool underscore = false;
 
-            Console.WriteLine("Bienvenue sur le jeu du pendu"+"\n vous avez 7 vie pour pour trouver le mot");
-
-            while (vie>0 && motTrouver==false)
-            {
-                for(int i=0;i<tabJeu.Length;i++)
-                {
-                    Console.Write(tabJeu[i] + " ");
-                }
-                Console.WriteLine("quelle lettre voulez-vous essayer?");
-                char lettre = Convert.ToChar(Console.ReadLine());
-                for(int i=0;i<tabJeu.Length;i++)
-                {
-                   if(motATrouve[i]==lettre)
-                    {
-                        tabJeu[i] = lettre;
-                        lettreTrouver = true;
-                    }
-                  
-                }
-                if(lettreTrouver==false)
-                {
-                    vie--;
-                    switch(vie)
-                    {
-                        case 0: Console.WriteLine(vie0); break;
-                        case 1: Console.WriteLine(vie1); break;
-                        case 2: Console.WriteLine(vie2); break;
-                        case 3: Console.WriteLine(vie3); break;
-                        case 4: Console.WriteLine(vie4); break;
-                        case 5: Console.WriteLine(vie5); break;
-                        case 6: Console.WriteLine(vie6); break;
-                    }
-                }
-                else
-                {
-                    if (underscore == false)
-                    {
-                        for (int i = 0; i < tabJeu.Length; i++)
-                        {
-                            if (tabJeu[i] == '_')
-                            {
-                                underscore = true;
-                            }
-                            if (underscore == false)
-                            {
-                                motTrouver = true;
-                            }
-                        }
-                    }
-                }
-            }
-            Console.WriteLine("Bravo vous avez gagner!!");
+            Console.ReadKey();
         }
     }
 }
